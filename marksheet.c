a2fb3da1531a1abf88140eae7eff08f42d6ec48d
vaishu50409-crypto <vaishu50409@gmail.com>
2026-09-19 20:38:01 +0530

    Create marksheet.c

---
+++ marksheet.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int marks = 85;
+    if(marks >= 90) printf("Grade A");
+    else if(marks >= 75) printf("Grade B");
+    else printf("Grade C");
+    return 0;
+}
